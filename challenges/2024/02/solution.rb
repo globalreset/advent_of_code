09f98a8f1d811e42cd111c5198aec150a8b65b2f
Scott Hughes <shughes@pobox.com>
2024-12-02 00:24:23 -0600

    tried to get too clever on part 2
    wasted a lot of time trying to avoid the brute force search on part 2
    when it took no time at all

---
+++ challenges/2024/02/solution.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+module Year2024
+  class Day02 < Solution
+
+    def check_report(report)
+      diffs = report.each_cons(2).map{ |a, b| b - a }
+      diffs.all? { |v| v!=0 && v.abs <= 3 && v.positive? == diffs.first.positive?  && v.negative? == diffs.first.negative? }
+    end
+
+    def part_1
+      data.map{ |report| check_report(report) }.count(true)
+    end
+
+    def part_2
+      data.map{ |report|
+        check_report(report) || (0...report.size).any?{ |i| check_report(report[0...i] + report[i+1..]) } 
+      }.count(true)
+    end
+
+    private
+      def process_input(line)
+        line.split(" ").map(&:to_i)
+      end
+  end
+end
