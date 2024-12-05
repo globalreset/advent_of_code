cdc3fc3eacd3219524b08b6e20ebf24961c3bef8
Scott Hughes <shughes@pobox.com>
2024-12-05 00:34:47 -0600

    2024 day 5
    I don't like that I went through the print data twice in part 2. but it
    felt like the most readable approach. Also, I originally calculated
    "before rules" and "after rules" thinking I would have to 'squeeze' a
    value in between the result of the two rule checks... But after I was
    done I found (through testing) that the original rule format was
    sufficent.

---
+++ challenges/2024/05/solution.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+module Year2024
+  class Day05 < Solution
+
+    def part_1
+      data["print"].map { |print|
+        pass = print.each_with_index.all? { |n, i|
+          (data["rules"][n]||[]).map { |r|
+            print.find_index(r)
+          }.compact.all? { |ri| ri > i }
+        }
+        next if (!pass)
+        print[print.size/2]
+      }.compact.sum
+    end
+
+    def part_2
+      data["print"].map { |print|
+        pass = print.each_with_index.all? { |n, i|
+          (data["rules"][n]||[]).map { |r|
+            print.find_index(r)
+          }.compact.all? { |ri| ri > i }
+        }
+        next if (pass)
+        newPrint = []
+        print.each { |n|
+          newIdx = (data["rules"][n]||[]).map{ |a| 
+            newPrint.find_index(a) 
+          }.compact.min || newPrint.size
+          newPrint.insert(newIdx, n)
+        }
+        newPrint[newPrint.size/2]
+      }.compact.sum
+    end
+
+    private
+      def process_dataset(set)
+        tmpRules, tmpPrint = set.slice_when{ |l| l.size==0 }.map(&:compact)
+        set = {"rules" => {}, "print" => []}
+        tmpRules.map{ |l| l.split("|").map(&:to_i) }.each { |a,b| 
+          (set["rules"][a] ||= []) << b
+        }
+        set["print"] = tmpPrint.map{ |l| l.split(",").map(&:to_i) }
+        set
+      end
+  end
+end
