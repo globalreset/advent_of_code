fc7881f85b818d489ffd14744284be65fbf642ee
Scott Hughes <shughes@pobox.com>
2024-12-08 02:04:49 -0600

    represent x,y as complex numbers to see how tight we can get it

--- challenges/2024/08/solution.rb
+++ challenges/2024/08/solution.rb
@@ -1,64 +1,47 @@
 # frozen_string_literal: true
 module Year2024
   class Day08 < Solution
-    # @input is available if you need the raw data input
-    # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
+    def in_bounds?(max, pos)
+      pos.real >= 0 && pos.real < max.real && pos.imag >= 0 && pos.imag < max.imag
+    end
 
     def part_1
-      grid, antenna = data
-      antinodes = Set.new
-      antenna.values.each { |coords|
-        coords.combination(2).each { |a1, a2|
-          dy = (a2[0] - a1[0])
-          dx = (a2[1] - a1[1])
-
-          anti1 = [a1[0] - dy, a1[1] - dx]
-          anti2 = [a2[0] + dy, a2[1] + dx]
-
-          antinodes << anti1 if(grid[anti1]!=nil)
-          antinodes << anti2 if(grid[anti2]!=nil)
-
-          (grid[anti1]!=nil ? 1 : 0) + (grid[anti2]!=nil ? 1 : 0)
+      max, antenna = data
+      antenna.each.with_object(Set.new) { |coords, antinodes|
+        # permutation does both directions, so get one vector (b-a)
+        # and find the next node along that vector. We'll get opposite
+        # vector on another permutation
+        coords.permutation(2).each { |a, b|
+          c = a - (b - a)
+          antinodes << c if(in_bounds?(max, c))
         }
-      }
-      antinodes.size
+      }.size
     end
 
     def part_2
-      grid, antenna = data
-      antinodes = Set.new
-      antenna.each { |ant,coords|
-        coords.combination(2).each { |a1, a2|
-          dy = (a2[0] - a1[0])
-          dx = (a2[1] - a1[1])
-
-          [1,-1].each { |dir|
-            step = 0
-            loop do
-              anti1 = [a1[0] + dir*step*dy, a1[1] + dir*step*dx]
-              break if(grid[anti1]==nil)
-              antinodes << anti1
-              step += 1
-            end 
+      max, antenna = data
+      antenna.each.with_object(Set.new) { |coords, antinodes|
+        coords.permutation(2).each { |a, b|
+          (0..).each { |step|
+            anti = a + step*(b - a)
+            break unless in_bounds?(max, anti)
+            antinodes << anti
           }
         }
-      }
-      antinodes.size
+      }.size
     end
 
     private
+      # return max pos in form of complex number
+      # and a hash array where key is antenna char
+      # and value is array of coords (as complex numbers)
       def process_dataset(set)
-        grid = {}
-        antenna = {}
-        set.each_with_index { |row, y|
-          row.chars.each_with_index { |col, x|
-            if(col != ".")
-              (antenna[col] ||= []) << [y, x]
-            end
-            grid[[y, x]] = col
-          }
-        }
-        [grid, antenna]
+        [ (set.size + set[0].size*1i), 
+          set.each_with_index.with_object({}) { |(row, y), hash|
+            row.chars.each_with_index { |col, x| 
+              (hash[col] ||= []) << y + x*1i if(col != ".") 
+            }
+          }.values ]
       end
   end
 end
