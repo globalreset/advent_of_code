cd53f3b190383f49ffc0d0854ee538af5eb9c84c
Scott Hughes <shughes@pobox.com>
2024-12-19 00:46:41 -0600

    2024 day 19

---
+++ challenges/2024/19/solution.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+module Year2024
+  class Day19 < Solution
+    def part_1
+      pattern = /^#{Regexp.union(*data[0].split(", "))}+$/
+      data[2..].count { |design| design =~ pattern }
+    end
+
+    def count_paths(design, pattern, cache)
+      return 1 if design.empty?
+
+      cache[design] ||= 
+        pattern.select { |p| design.start_with?(p) }
+               .sum { |p| count_paths(design[p.length..], pattern, cache) }
+    end
+
+    def part_2
+      pattern = data[0].split(", ")
+      cache = {}
+      data[2..].sum { |design| count_paths(design, pattern, cache) }
+    end
+  end
+end
