fbc05bd2e44da261dac6b8dca88a4e7c9bc578a6
Scott Hughes <shughes@pobox.com>
2024-12-20 01:41:40 -0600

    day 20 small optimizations for fun

--- challenges/2024/20/solution.rb
+++ challenges/2024/20/solution.rb
@@ -1,50 +1,59 @@
 # frozen_string_literal: true
 module Year2024
   class Day20 < Solution
-    def find_path(maxCheat = 2)
+    def find_path
       start = data.select { |k, v| v == 'S' }.keys.first
       goal = data.select { |k, v| v == 'E' }.keys.first
       walls = data.select { |k, v| v == '#' }.keys.to_set
       
-      # Find distance from start for every position
       stepCnt = { start => 0 }
+      visited = Set.new
 
       queue = [start]
       until queue.empty?
         pos = queue.shift
+        visited << pos
         steps = stepCnt[pos]
         
         [Vector[-1,0], Vector[1,0], Vector[0,-1], Vector[0,1]].each do |d|
           newPos = pos + d
-          next if (walls.include?(newPos) || stepCnt.key?(newPos))
+          next if (walls.include?(newPos) || visited.include?(newPos))
           stepCnt[newPos] = steps + 1
           queue << newPos
         end
       end
-     
-      # visit all positions on the base path
-      stepCnt.to_a.sum do |pos, steps|
-        # check every x and y adjustment possible for cheat
-        (-maxCheat..maxCheat).sum do |dx|
-          (-maxCheat..maxCheat).count do |dy|
-            newPos = pos + Vector[dx, dy]
-            next if (dx==0 && dy==0) 
-            next if (dx.abs + dy.abs > maxCheat)
-            next unless stepCnt.key?(newPos)
-            
-            # See if cheating to that position is better by at least 100
-            stepCnt[newPos] - (steps + (dx.abs + dy.abs)) >= 100
-          end
-        end
-      end
+      stepCnt
     end
     
     def part_1
-      find_path(2)
+      maxCheat = 2
+      stepCnt = find_path()
+      stepCnt.to_a.sum { |pos, steps|
+        # part 1 is faster to check all points on grid by max distance
+        (-maxCheat..maxCheat).sum { |dx|
+          (-maxCheat..maxCheat).count { |dy|
+            pos2 = pos + Vector[dx, dy]
+            next if (dx==0 && dy==0) 
+            next if (dx.abs + dy.abs > maxCheat)
+            next unless stepCnt.key?(pos2)
+            stepCnt[pos2] - (steps + (dx.abs + dy.abs)) >= 100
+          }
+        }
+      }
     end
     
     def part_2
-      find_path(20)
+      maxCheat = 20
+      stepCnt = find_path()
+      points = stepCnt.keys.to_set
+      stepCnt.to_a.sum { |pos1, step1|
+        # part 2 is faster to check each pair of points on the path
+        points.delete(pos1).count{ |pos2|
+          cheat = (pos2[0] - pos1[0]).abs + (pos2[1] - pos1[1]).abs
+          next if cheat > maxCheat
+          stepCnt[pos2] - (step1 + cheat) >= 100
+        }
+      }
     end
 
     private
